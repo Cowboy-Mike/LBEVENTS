30c5f2532b2c0406ecc72c626ce6764f65f9393e
Mike Nanto <mike@ascusa.com>
2021-03-20 15:56:39 -0700

    file structure set up.

---
+++ lib/longbeach_events.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "./longbeach_events/version"
+require_relative "./longbeach_events/cli"
+
+module LongbeachEvents
+  class Error < StandardError; end
+  # Your code goes here...
+end
